31339f9bf0bb6f4ef2a8c52716a727d9de027301
Zachary Belles <91072653+zmbelles@users.noreply.github.com>
2023-04-26 19:37:19 -0500

    Update parse-form-file.ts

--- parse-form-file.ts
+++ parse-form-file.ts
@@ -1,33 +1,59 @@
-import type { NextApiRequest } from "next";
-import { IncomingForm, File } from "formidable";
-import fs from "fs/promises";
+import { NextApiRequest, NextApiResponse } from "next";
+import mime from "mime";
+import { join } from "path";
+import * as dateFn from "date-fns";
+import formidable from "formidable";
+import { mkdir, stat } from "fs/promises";
+export const FormidableError = formidable.errors.FormidableError;
 
-export async function parseFormFile(
-  req: NextApiRequest,
-  fieldName: string
-): Promise<File | undefined> {
-  const form = new IncomingForm({ multiples: false });
-  const files: { [key: string]: File } = {};
+export const parseForm = async (
+  req: NextApiRequest
+): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
+  return new Promise(async (resolve, reject) => {
+    const uploadDir = join(
+      process.cwd(),
+      `/uploads/${dateFn.format(Date.now(), "dd-MM-Y")}`
+    );
 
-  form.on("file", (field, file) => {
-      files[field] = file;
-  });
-
-  await new Promise<void>((resolve, reject) => {
-    form.parse(req, (err, fields, _) => {
-      if (err) {
-        reject(err);
+    try {
+      await stat(uploadDir);
+    } catch (error: any) {
+      if (error.code === "ENOENT") {
+        await mkdir(uploadDir, { recursive: true });
+      } else {
+        console.log(error);
+        reject(error);
         return;
       }
+    }
 
-      resolve();
+    const form = formidable({
+      maxFiles: 2,
+      maxFileSize: 10 * 1024 * 1024, // 10MB
+      uploadDir,
+      filename: (_name, _ext, part) => {
+        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+        const filename = `${part.name || "unknown"}-${uniqueSuffix}.${
+          mime.getExtension(part.mimetype || "") || "unknown"
+        }`;
+        return filename;
+      },
+      filter: (part) => {
+        return (
+          part.name === "file" && (part.mimetype?.includes("image") || false)
+        );
+      },
+    });
+
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err);
+      else {
+        if (req.body)
+          Object.entries(req.body).forEach(([key, value]) => {
+            fields[key] = value as string;
+          });
+        resolve({ fields, files });
+      }
     });
   });
-  const file = files[fieldName];
-  if (file) {
-    const data = await fs.readFile(file.filepath);
-    console.log(`Data: ${data}`); 
-  }
-  console.log(`File ${file}`)
-  return file;
-}
+};
