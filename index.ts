84d14e78beddc536671cab3902557a07227c243f
Zachary Belles <91072653+zmbelles@users.noreply.github.com>
2023-04-26 19:36:56 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,41 +1,152 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { S3Client } from '@aws-sdk/client-s3';
-import { parseFormFile } from "@/parse-form-file";
-import AWS from "aws-sdk";
-import { uploadFileToS3 } from "@/amazon-s3";
+import connectDB from "@/lib/mongodb";
+import { getAuthUser } from "@/lib/auth";
+import { uploadFileToS3, updateFileOnS3 } from "@/lib/amazon-s3";
+import formidable from "formidable";
+import FormData from "form-data";
+import fs from "fs";
+import Post, { IPost } from "@/models/Post";
+import { parseForm } from "@/lib/parseForm";
 
-//s3 instance for getSignedUrl()
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
-
-//s3 client to upload using multers3
-const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "DEFAULT",
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "DEFAULT",
+// IMPORTANT: Prevents next from trying to parse the form
+export const config = {
+  api: {
+    bodyParser: false,
   },
-});
-
+};
+type formidableData = {
+  fields: formidable.Fields;
+  files: formidable.Files;
+};
+interface uploadFile {
+  success: boolean;
+  signedUrl?: string;
+  message: string;
+  key?: string;
+}
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { method } = req;
 
+  await connectDB();
+
   switch (method) {
-    case "POST":
-    {
-        const file = await parseFormFile(req, "file");
-	if(file === null){
-		res.status(400).json({ success: false, httpStatus: "Bad Request" });
-	}
-        console.log(file);
-        const url = await uploadFileToS3(file);
-        res.status(201).json({ success: true, httpStatus: "Accepted", data: url})
-  	}
+    case "GET": {
+      const data = req.query.data;
+      res.status(200).json({ success: true, httpStatus: "200", data: data });
     }
+    case "PUT": {
+      try {
+        const { fields, files }: formidableData = await parseForm(req);
+        const { post_id, visibility, body, link, originalFilename } = fields;
+        const parsedFile: formidable.File = files.file as formidable.File;
+
+        console.log("parsed file");
+        let url,
+          newFilename,
+          newURL: string = "";
+        if (!post_id) {
+          return res.status(400).json({
+            success: false,
+            message: "postId is required to update the post",
+          });
+        }
+
+        const postToUpdate = await Post.findById(post_id);
+        if (!postToUpdate) {
+          res.status(404).json({ error: "Post not found" });
+        }
+        console.log(`Post: ${postToUpdate}`);
+        if (visibility) postToUpdate.visibility = visibility;
+        if (body) postToUpdate.content.body = body;
+        if (link) postToUpdate.content.link = link;
+
+        console.log("easy changes made");
+        if (parsedFile) {
+          url = parsedFile.filepath;
+          const buffer: Buffer = fs.readFileSync(url);
+          newFilename = parsedFile.originalFilename;
+          newURL = await updateFileOnS3(buffer, originalFilename as string);
+          postToUpdate.content.image.signURL = newURL;
+          postToUpdate.content.image.filename = newFilename;
+        }
+        const post = postToUpdate.save();
+        res.status(200).json({ sucess: true, Post: post });
+      } catch (e) {
+        res.status(400).json({ error: e });
+      }
+    }
+    case "POST": {
+      try {
+        //grab the file and fields
+        const { fields, files }: formidableData = await parseForm(req);
+        const {
+          username,
+          email,
+          body,
+          user_id,
+          community_id,
+          main_platform,
+          facebook_post_id,
+          instagram_post_id,
+          visibility,
+          link,
+        }: formidable.Fields = fields;
+        const parsedFile: formidable.File = files.file as formidable.File;
+
+        //if there is a file, grab data
+        if (parsedFile) {
+          // Create image post
+          const url: string = parsedFile.filepath;
+          const buffer: Buffer = fs.readFileSync(url);
+          let signedURL: string = "";
+
+          //check for buffer failure
+          if (!buffer) {
+            res.status(400).json({
+              success: false,
+              error: "Could not parse buffer from file",
+            });
+          }
+
+          signedURL = await uploadFileToS3(
+            buffer,
+            parsedFile.originalFilename!
+          );
+          if (signedURL === "") {
+            res
+              .status(500)
+              .json({ status: false, message: "could not upload to S3" });
+          }
+          const newPost: IPost = new Post({
+            user_id: user_id,
+            username: username,
+            email: email,
+            community_id: community_id,
+            main_platform: main_platform,
+            content: {
+              body: body,
+              image: {
+                signUrl: signedURL,
+                originalFilename: parsedFile.originalFilename,
+              },
+            },
+            visibility: visibility,
+          });
+          newPost.save();
+          res.status(200).json({
+            status: true,
+            Post: newPost,
+          });
+        } else {
+        }
+      } catch (err) {
+        res
+          .status(401)
+          .json({ status: false, message: "could not parse file" });
+      }
+    }
+  }
 }
